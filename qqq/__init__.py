0e01aef55e3bebfef0a510cb3f8d8cccf46cc3f7
AradGolbaghi <arad6432@gmail.com>
2023-07-04 19:06:14 +0100

    Formatted string.

--- qqq/__init__.py
+++ qqq/__init__.py
@@ -8,10 +8,12 @@ while play:
         print("\nSorry but the toys are not worth $" + str(box_price))
         print("How much would you want to sell this box of toys for? ")
 
-    if box_price > str(20):
+    if box_price < str(20):
         print("\nWell let's wait until someone is here for the box of toys :) ")
         print("And then we would be $" + box_price + " richer ")
-    else: print("If wanted I can Put a random number in (y/n) ")
-    if 'y':
-        box_price = random.randint(0, 20)
+    else:
+        print("If wanted I can Put a random number in (y/n) ")
+
+
         
+
