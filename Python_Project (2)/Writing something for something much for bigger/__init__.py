1d90cb172e8c0ce57d964109747a7405e57514bc
AradGolbaghi <arad6432@gmail.com>
2023-07-05 16:01:50 +0100

    Writing something for something much moe bigger

---
+++ Python_Project (2)/Writing something for something much for bigger/__init__.py
@@ -0,0 +1,9 @@
+print('Hello')
+welcome = "Welcome to my (github) account "
+print(welcome)
+hope = "I hope that you like the coding that I do "
+print(hope)
+yt = "\n and if you want to really support an 11 year old kid you can go on youtube and sub to my channel," \
+     " \nwhich this is the link to (https://www.youtube.com/channel/UCvu9DyTP5jHu4vWFdgLqvpA) Thank you "
+print(yt)
+print('\n Thank you for reading. ')
