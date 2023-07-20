bfae76247b55cb62c0778162add46651860baaf6
AradGolbaghi <arad6432@gmail.com>
2023-07-20 16:10:46 +0100

    if statements

--- Strings Methods/Strings Methods/__init__.py
+++ Strings Methods/Strings Methods/__init__.py
@@ -1,4 +1,9 @@
-course = "Python for Beginners "
-print("Python" in course)
-print("for" in course)
-print("Beginners" in course)
+buyer_has_good_credit = True
+
+price_house = 1000000
+
+if buyer_has_good_credit:
+    down_payment = 0.1 * price_house
+else:
+    down_payment = 0.2 * price_house
+print(f"down_payment: ${down_payment}")
