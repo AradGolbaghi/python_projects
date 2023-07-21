09b09a70ac19d1bb6f68c70016b7ef56f4d15c0f
AradGolbaghi <arad6432@gmail.com>
2023-07-21 17:40:04 +0100

    Play my game RACE NOW

--- Strings Methods/Strings Methods/__init__.py
+++ Strings Methods/Strings Methods/__init__.py
@@ -1,8 +1,8 @@
 weight = int(input("Weight: "))
 unit = input('(L)bs or (K)g: ')
 if unit.upper() == 'L':
-    converted = weight * 0.45
+    converted = weight * 2.20462262
     print(f"You are {converted} kilograms")
 else:
-    converted = weight / 0.45
+    converted = weight / 2.20462262
     print(f"You are {converted} lbs")
