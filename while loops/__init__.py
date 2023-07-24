434848576a2f62c97ef1826a341f0b42cb8bb5bb
AradGolbaghi <arad6432@gmail.com>
2023-07-24 11:23:32 +0100

    Play my game RACE NOW (second version)

--- while loops/__init__.py
+++ while loops/__init__.py
@@ -1,18 +1,26 @@
-homme = ""
-
+hommee = ""
+started = False
 while True:
-    homme = input("> ").lower()
-    if homme == "start":
+    hommee = input("> ").lower()
+    if hommee == "start":
+        if started:
+           print("Car is already started")
+        else:
+            started = True
         print("Car started, Go Go Go! ")
-    elif homme == "stop":
-        print("Car stopped oh no!")
-    elif homme == "help":
+    elif hommee == "stop":
+        if not started:
+            print("Car is already stopped")
+        else:
+            started = False
+            print("Car stopped oh no!")
+    elif hommee == "help":
         print("""
 Start = Car started, Go Go Go!
 Stop = Car stopped oh no!
 Quit = Exiting the game
 """)
-    elif homme == "quit":
+    elif hommee == "quit":
         break
     else:
         print("Sorry, My programmer didn't tell me what do those words mean :( ")
