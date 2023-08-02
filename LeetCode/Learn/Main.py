9ea831a884bb627ffa75608da6758d82a392ac1a
AradGolbaghi <arad6432@gmail.com>
2023-08-02 16:34:09 +0100

    number multiplied by -1

---
+++ LeetCode/Learn/Main.py
@@ -0,0 +1,7 @@
+# wriht a function that takes a NUMBER  as a parameter and retrunts the numer multiplied by -1
+
+def multiply_by(number):
+    return number * -1
+
+multiply_by(2)
+print(multiply_by(2))
---
+++ LeetCode/Learn/returningFirstElemnt.py
@@ -0,0 +1,12 @@
+## make a function that returns the first elemnt in an integer array
+
+##  [1,2,3,4] -> 1
+## [12,3,7] -> 12 
+
+
+def element(elemtList):
+    return elemtList[0]
+
+
+print(element([12,2,3,4] ))
+    
